fc7dad847426f562be4255508a9217788cfb8b5a
Vlastimil Zeman <vlastimil@zeman.dev>
2021-07-23 12:09:09 +0100

    PSEC-1268 Remove all temp files of prowler

--- install_prowler.sh
+++ install_prowler.sh
@@ -5,11 +5,11 @@ PROWLER_VERSION='2.4.1'
 
 rm -rf prowler.zip
 rm -rf src/platsec/compliance/lib/prowler/*
-rm -rf prowler-${PROWLER_VERSION}
+rm -rf "prowler-${PROWLER_VERSION}"
 
-wget https://github.com/toniblyx/prowler/archive/refs/tags/${PROWLER_VERSION}.zip -O prowler.zip
+wget "https://github.com/toniblyx/prowler/archive/refs/tags/${PROWLER_VERSION}.zip" -O prowler.zip
 unzip prowler.zip
 
 mkdir -p src/platsec/compliance/lib/prowler
-mv prowler-${PROWLER_VERSION}/* src/platsec/compliance/lib/prowler
-rm prowler.zip
+mv "prowler-${PROWLER_VERSION}/"* src/platsec/compliance/lib/prowler
+rm -rf prowler.zip "prowler-${PROWLER_VERSION}"
